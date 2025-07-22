f161422d1374d6c78d380bcdfd00289f7c6d8a47
Alex Jorns <akjorns@icloud.com>
2025-07-21 19:45:10 -0500

    added data and cleaned up some elements

--- src/App.svelte
+++ src/App.svelte
@@ -33,9 +33,60 @@
 
   // Hardcoded data for testing (comment out when loading CSV)
   data.region.indicators = [
-    { code: "01", name: "Alabama", education: 35, poverty: 500, pop: 5000, region: "Deep South" },
-    { code: "02", name: "Georgia", education: 40, poverty: 450, pop: 6000, region: "Deep South" }
-  ];
+  { code: "01", name: "Alabama", education: 40, poverty: 15.6, pop: 27, region: "Deep South" },
+  { code: "02", name: "Alaska", education: 28, poverty: 10.4, pop: 4, region: "West" },
+  { code: "04", name: "Arizona", education: 31, poverty: 12.4, pop: 6, region: "West" },
+  { code: "05", name: "Arkansas", education: 31, poverty: 15.7, pop: 16, region: "Deep South" },
+  { code: "06", name: "California", education: 35, poverty: 12.0, pop: 6, region: "West" },
+  { code: "08", name: "Colorado", education: 23, poverty: 9.3, pop: 5, region: "West" },
+  { code: "09", name: "Connecticut", education: 18, poverty: 10.3, pop: 13, region: "Northeast" },
+  { code: "10", name: "Delaware", education: 29, poverty: 10.5, pop: 24, region: "South" },
+  { code: "11", name: "District of Columbia", education: 46, poverty: 14.0, pop: 44, region: "South" },
+  { code: "12", name: "Florida", education: 30, poverty: 12.3, pop: 17, region: "South" },
+  { code: "13", name: "Georgia", education: 32, poverty: 13.6, pop: 33, region: "Deep South" },
+  { code: "15", name: "Hawaii", education: 28, poverty: 10.1, pop: 2, region: "West" },
+  { code: "16", name: "Idaho", education: 22, poverty: 10.1, pop: 1, region: "West" },
+  { code: "17", name: "Illinois", education: 26, poverty: 11.6, pop: 15, region: "Midwest" },
+  { code: "18", name: "Indiana", education: 23, poverty: 12.3, pop: 10, region: "Midwest" },
+  { code: "19", name: "Iowa", education: 17, poverty: 11.3, pop: 5, region: "Midwest" },
+  { code: "20", name: "Kansas", education: 21, poverty: 11.2, pop: 6, region: "Midwest" },
+  { code: "21", name: "Kentucky", education: 29, poverty: 16.4, pop: 9, region: "South" },
+  { code: "22", name: "Louisiana", education: 36, poverty: 18.9, pop: 33, region: "Deep South" },
+  { code: "23", name: "Maine", education: 22, poverty: 10.4, pop: 2, region: "Northeast" },
+  { code: "24", name: "Maryland", education: 26, poverty: 9.5, pop: 32, region: "South" },
+  { code: "25", name: "Massachusetts", education: 14, poverty: 10.4, pop: 10, region: "Northeast" },
+  { code: "26", name: "Michigan", education: 30, poverty: 13.5, pop: 14, region: "Midwest" },
+  { code: "27", name: "Minnesota", education: 17, poverty: 9.3, pop: 8, region: "Midwest" },
+  { code: "28", name: "Mississippi", education: 39, poverty: 18.0, pop: 38, region: "Deep South" },
+  { code: "29", name: "Missouri", education: 26, poverty: 12.0, pop: 12, region: "Midwest" },
+  { code: "30", name: "Montana", education: 20, poverty: 11.7, pop: 1, region: "West" },
+  { code: "31", name: "Nebraska", education: 24, poverty: 10.5, pop: 5, region: "Midwest" },
+  { code: "32", name: "Nevada", education: 32, poverty: 12.0, pop: 11, region: "West" },
+  { code: "33", name: "New Hampshire", education: 16, poverty: 7.2, pop: 2, region: "Northeast" },
+  { code: "34", name: "New Jersey", education: 18, poverty: 9.7, pop: 16, region: "Northeast" },
+  { code: "35", name: "New Mexico", education: 37, poverty: 17.8, pop: 3, region: "West" },
+  { code: "36", name: "New York", education: 28, poverty: 14.2, pop: 18, region: "Northeast" },
+  { code: "37", name: "North Carolina", education: 25, poverty: 12.8, pop: 22, region: "South" },
+  { code: "38", name: "North Dakota", education: 18, poverty: 9.8, pop: 4, region: "Midwest" },
+  { code: "39", name: "Ohio", education: 21, poverty: 13.3, pop: 13, region: "Midwest" },
+  { code: "40", name: "Oklahoma", education: 31, poverty: 15.9, pop: 8, region: "South" },
+  { code: "41", name: "Oregon", education: 27, poverty: 12.2, pop: 2, region: "West" },
+  { code: "42", name: "Pennsylvania", education: 22, poverty: 12.0, pop: 12, region: "Northeast" },
+  { code: "44", name: "Rhode Island", education: 26, poverty: 10.8, pop: 9, region: "Northeast" },
+  { code: "45", name: "South Carolina", education: 31, poverty: 13.9, pop: 26, region: "Deep South" },
+  { code: "46", name: "South Dakota", education: 21, poverty: 11.8, pop: 3, region: "Midwest" },
+  { code: "47", name: "Tennessee", education: 31, poverty: 14.0, pop: 17, region: "Deep South" },
+  { code: "48", name: "Texas", education: 20, poverty: 13.7, pop: 14, region: "Deep South" },
+  { code: "49", name: "Utah", education: 25, poverty: 9.0, pop: 2, region: "West" },
+  { code: "50", name: "Vermont", education: 16, poverty: 9.7, pop: 2, region: "Northeast" },
+  { code: "51", name: "Virginia", education: 23, poverty: 10.2, pop: 20, region: "South" },
+  { code: "53", name: "Washington", education: 21, poverty: 10.3, pop: 5, region: "West" },
+  { code: "54", name: "West Virginia", education: 35, poverty: 16.7, pop: 4, region: "South" },
+  { code: "55", name: "Wisconsin", education: 22, poverty: 10.7, pop: 7, region: "Midwest" },
+  { code: "56", name: "Wyoming", education: 19, poverty: 11.3, pop: 1, region: "West" }
+];
+
+
 
   // Reactive statement: get region based on selected code (fixed to avoid metadata lookup)
   $: region = selected
@@ -164,10 +215,9 @@
               {xKey} {yKey} {zKey} {rKey}
               idKey="code" labelKey="name"
               r={[3, 10]}
-              xScale="log"
-              xTicks={[10, 100, 1000, 10000]}
-              xFormatTick={d => d.toLocaleString()}
-              xSuffix=" sq.km"
+              xScale="linear"
+				xTicks={[0, 20, 40, 60, 80, 100]}
+				xFormatTick={d => `${d}%`}
               yFormatTick={d => d.toLocaleString()}
               legend={zKey != null}
               labels
